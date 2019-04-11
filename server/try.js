b199699ee6edf54fce94335ea2eeba9ca8bcbac4
Volodymyr Kryvko <vkryv@softserveinc.com>
2019-04-11 16:14:48 +0300

    add async fuтсешщті

--- server/try.js
+++ server/try.js
@@ -1,29 +1,74 @@
 const sql = require('mssql')
- 
-// async/await style:
-const pool = new sql.ConnectionPool({
-    server: 'DESKTOP-SB9SU5F',
-    user: 'kryvkovr',
-    password: '1',
-    database: 'master',
-    port:1433,
-    options:{
-        encrypt:false
-    }
-}).connect();
- 
 
-async function messageHandler() {
-    await pool; // ensures that the pool has been created
-    try {
-        console.log(pool)
-    	const request = new pool.request(); // or: new sql.Request(pool1)
-    	const result = request.query('select 1 as number')
-    	console.dir(result)
-    	return result;
-    } catch (err) {
-        console.error('SQL error', err);
+
+const config = {
+    server: 'TULSA',
+    user: 'tut',
+    password: '1',
+    database: 'tutorial',
+    port: 1433,
+    options: {
+        encrypt: false
     }
 }
 
-messageHandler()
+const connection = new sql.ConnectionPool(config)
+
+
+const selectStudents = async () => {
+    const pool = await connection.connect();
+    const sqlQuery = "select * from Student"
+    const request = new sql.Request(pool); // or: new sql.Request(pool1)
+    const result= await request.query(sqlQuery);
+    console.log(result);
+}
+
+setTimeout(() => {
+    selectStudents();
+}, 1000)
+
+// connectToDataBase();
+
+// console.log(pool.)
+
+
+// async function messageHandler() {
+//     await pool; // ensures that the pool has been created
+//     try {
+//         // console.log(pool)
+//         const request = new sql.Request(pool);
+
+
+//         request.query('select * Student', (err, result) => {
+//             // ... error checks
+
+//             console.log(result) // return 1
+
+//             // ...
+//         })
+
+//     	// const result = await request.query('select * from Country')
+//         // console.log(result)
+//         // process.exit();
+
+
+
+
+
+//     } catch (err) {
+//         console.error('SQL error', err);
+//     }
+// }
+
+// messageHandler()
+
+
+// new sql.ConnectionPool(config).connect()
+//     .then(pool => {
+//         return pool.query`select * from Student`
+//     }).then(result => {
+//         console.log(result)
+//         process.exit()
+//     }).catch(err => {
+//         // ... error checks
+//     })
